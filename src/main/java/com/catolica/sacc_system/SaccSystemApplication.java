fc324ddccd137a720763a8d3a5a4b616a8344800
tiagoczz <tiagoeliassilva2005@gmail.com>
2024-11-08 20:42:58 -0300

    primeiros templates

---
+++ src/main/java/com/catolica/sacc_system/SaccSystemApplication.java
@@ -0,0 +1,13 @@
+package com.catolica.sacc_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaccSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaccSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catolica/sacc_system/SaccSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catolica.sacc_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaccSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

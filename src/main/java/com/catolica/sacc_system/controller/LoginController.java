a2a5c659cde24e76ba319bb1ece3bfb649e1ac54
tiagoczz <tiagoeliassilva2005@gmail.com>
2024-11-10 13:58:12 -0300

    melhorando templates e rederizando-os

---
+++ src/main/java/com/catolica/sacc_system/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.catolica.sacc_system.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("register")
+    public String register() {
+        return "register";
+    }
+
+}
